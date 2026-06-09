2964c7f5cfc5674764f8e3ad9260d3b7b5635eb8
Facundo Etchart <etchartfacu@gmail.com>
2026-06-09 17:09:25 -0300

    Corrige el mensaje por pantalla
    Corrige el mensaje por pantalla en el caso de que se cancela el reclamo.

--- eBarrio/src/reclamos/estados/EstadoAsignado.java
+++ eBarrio/src/reclamos/estados/EstadoAsignado.java
@@ -3,6 +3,7 @@ package reclamos.estados;
 import reclamos.reclamo.Reclamo;
 
 public class EstadoAsignado implements IEstadoReclamo{
+
     @Override
     public void avanzar(Reclamo reclamo) {
         System.out.println("El reclamo pasa de 'Asignado' a 'En proceso'.");
@@ -11,8 +12,8 @@ public class EstadoAsignado implements IEstadoReclamo{
 
     @Override
     public void cancelar(Reclamo reclamo) {
-        System.out.println("El reclamo que se encontraba asignado ha sido directamente cancelado");
-        reclamo.setEstado(new EstadoCancelado());
+        System.out.println("El reclamo que se encontraba ya asignado ha sido directamente cerrado.");
+        reclamo.setEstado(new EstadoCerrado());
     }
 
     @Override
